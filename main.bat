9de346ec214da52b8988d8f2e8b4adbd7bb67367
Thắng Lê <lnvthang.it@gmail.com>
2024-09-01 22:14:48 +0700

    first commit

---
+++ main.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+ECHO "Link github:"
+set /p url=""
+ECHO %url%
+
+@REM set /p choice= "" 
+@REM echo %choice%
+@REM SET content1=1999
+@REM SET message1=Message 1: %content1%
+@REM SET message2=Message 2: %1%
+@REM SET msg_error=Message Error
+@REM ECHO %DATE% - %TIME%
+@REM ECHO DEBUG: %msg_error%
+@REM ECHO %message1% - %message2% > D:\new.txt
+PAUSE
